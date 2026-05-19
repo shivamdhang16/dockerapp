077cd92a63e8c4443b09b0e141e2c017810527d1
root <root@ip-172-31-10-45.ap-south-1.compute.internal>
2026-05-19 04:59:38 +0000

    main branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Jay hind from india main branch !"
+    return "Jay hind from india main    branch !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
